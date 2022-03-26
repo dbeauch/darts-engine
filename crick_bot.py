7e342bc3baf9e7dcd4f3e09ea2b758037a9d1539
Duncan Beauch <97538022+dbeauch@users.noreply.github.com>
2022-03-26 16:41:52 -0400

    Add files via upload

---
+++ crick_bot.py
@@ -0,0 +1,11 @@
+import crick_game.py
+
+def findTarget1(boardState):
+    #for target in range(0, len(boardState)):
+
+
+def findTarget2(boardState):
+    return target
+
+def findTarget3(boardState):
+    return target
