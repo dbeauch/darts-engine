c8a2baa805adb5f49d7b069bb83538e1190b0b0c
KieranOdell <kfo7qb@virginia.edu>
2022-03-27 04:28:21 -0400

    GUI interface with values- 3 col

--- crick_game.py
+++ crick_game.py
@@ -1,154 +1,117 @@
-player1 = input("Enter player 1: ")
-player2 = input("Enter player 2: ")
+#player1 = input("Player 1 name: ")
+#player2 = input("Player 2 name: ")
+#players = [player1, player2]
 
-boardState1 = [2, 3, 3, 3, 3, 3, 3, 0]
-boardState2 = [3, 3, 3, 3, 3, 3, 2, 0]
+boardState1 = [0, 0, 0, 0, 0, 0, 0, 0]
+boardState2 = [0, 0, 0, 0, 0, 0, 0, 0]
+states = [boardState1, boardState2]
+
 turn = 0
 subTurn = 0
 
+boardHistory1 = []
+boardHistory2 = []
+boardHistories = [boardHistory1, boardHistory2]
 
-def updateBoard(throw):
-    if turn % 2 == 0:
-        if throw == "15":
-            if (boardState1[0] == 3) & (boardState2[0] != 3):
-                boardState1[7] += 15
-                return boardState1
-            elif (boardState1[0] == 3) & (boardState2[0] == 3):
-                return boardState1
-            boardState1[0] += 1
-        elif throw == "16":
-            if (boardState1[1] == 3) & (boardState2[1] != 3):
-                boardState1[7] += 16
-                return boardState1
-            elif (boardState1[1] == 3) & (boardState2[1] == 3):
-                return boardState1
-            boardState1[1] += 1
-        elif throw == "17":
-            if (boardState1[2] == 3) & (boardState2[2] != 3):
-                boardState1[7] += 17
-                return boardState1
-            elif (boardState1[2] == 3) & (boardState2[2] == 3):
-                return boardState1
-            boardState1[2] += 1
-        elif throw == "18":
-            if (boardState1[3] == 3) & (boardState2[3] != 3):
-                boardState1[7] += 18
-                return boardState1
-            elif (boardState1[3] == 3) & (boardState2[3] == 3):
-                return boardState1
-            boardState1[3] += 1
-        elif throw == "19":
-            if (boardState1[4] == 3) & (boardState2[4] != 3):
-                boardState1[7] += 19
-                return boardState1
-            elif (boardState1[4] == 3) & (boardState2[4] == 3):
-                return boardState1
-            boardState1[4] += 1
-        elif throw == "20":
-            if (boardState1[5] == 3) & (boardState2[5] != 3):
-                boardState1[7] += 20
-                return boardState1
-            elif (boardState1[5] == 3) & (boardState2[5] == 3):
-                return boardState1
-            boardState1[5] += 1
-        elif throw == "25":
-            if (boardState1[6] == 3) & (boardState2[6] != 3):
-                boardState1[7] += 25
-                return boardState1
-            elif (boardState1[6] == 3) & (boardState2[6] == 3):
-                return boardState1
-            boardState1[6] += 1
-        else:
-            return boardState1
+
+def updateBoard(value):
+    thrower = turn % 2
+    if thrower == 0:
+        boardHistory1.append(boardState1)
     else:
-        if throw == "15":
-            if (boardState2[0] == 3) & (boardState1[0] != 3):
-                boardState2[7] += 15
-                return boardState2
-            elif (boardState2[0] == 3) & (boardState1[0] == 3):
-                return boardState2
-            boardState2[0] += 1
-        elif throw == "16":
-            if (boardState2[1] == 3) & (boardState1[1] != 3):
-                boardState2[7] += 16
-                return boardState2
-            elif (boardState2[1] == 3) & (boardState1[1] == 3):
-                return boardState2
-            boardState2[1] += 1
-        elif throw == "17":
-            if (boardState2[2] == 3) & (boardState1[2] != 3):
-                boardState2[7] += 17
-                return boardState2
-            elif (boardState2[2] == 3) & (boardState1[2] == 3):
-                return boardState2
-            boardState2[2] += 1
-        elif throw == "18":
-            if (boardState2[3] == 3) & (boardState1[3] != 3):
-                boardState2[7] += 18
-                return boardState2
-            elif (boardState2[3] == 3) & (boardState1[3] == 3):
-                return boardState2
-            boardState2[3] += 1
-        elif throw == "19":
-            if (boardState2[4] == 3) & (boardState1[4] != 3):
-                boardState2[7] += 19
-                return boardState2
-            elif (boardState2[4] == 3) & (boardState1[4] == 3):
-                return boardState2
-            boardState2[4] += 1
-        elif throw == "20":
-            if (boardState2[5] == 3) & (boardState1[5] != 3):
-                boardState2[7] += 20
-                return boardState2
-            elif (boardState2[5] == 3) & (boardState1[5] == 3):
-                return boardState2
-            boardState2[5] += 1
-        elif throw == "25":
-            if (boardState2[6] == 3) & (boardState1[6] != 3):
-                boardState2[7] += 25
-                return boardState2
-            elif (boardState2[6] == 3) & (boardState1[6] == 3):
-                return boardState2
-            boardState2[6] += 1
-        else:
-            return boardState2
+        boardHistory2.append(boardState2)
+    if value == "15":
+        if (states[thrower][0] == 3) & (states[1 if thrower == 0 else 0][0] != 3):
+            states[thrower][7] += 15
+            return states[thrower]
+        elif (states[thrower][0] == 3) & (states[1 if thrower == 0 else 0][0] == 3):
+            return states[thrower]
+        states[thrower][0] += 1
+    elif value == "16":
+        if (states[thrower][1] == 3) & (states[1 if thrower == 0 else 0][1] != 3):
+            states[thrower][7] += 16
+            return states[thrower]
+        elif (states[thrower][1] == 3) & (states[1 if thrower == 0 else 0][1] == 3):
+            return states[thrower]
+        states[thrower][1] += 1
+    elif value == "17":
+        if (states[thrower][2] == 3) & (states[1 if thrower == 0 else 0][2] != 3):
+            states[thrower][7] += 17
+            return states[thrower]
+        elif (states[thrower][2] == 3) & (states[1 if thrower == 0 else 0][2] == 3):
+            return states[thrower]
+        states[thrower][2] += 1
+    elif value == "18":
+        if (states[thrower][3] == 3) & (states[1 if thrower == 0 else 0][3] != 3):
+            states[thrower][7] += 18
+            return states[thrower]
+        elif (states[thrower][3] == 3) & (states[1 if thrower == 0 else 0][3] == 3):
+            return states[thrower]
+        states[thrower][3] += 1
+    elif value == "19":
+        if (states[thrower][4] == 3) & (states[1 if thrower == 0 else 0][4] != 3):
+            states[thrower][7] += 19
+            return states[thrower]
+        elif (states[thrower][4] == 3) & (states[1 if thrower == 0 else 0][4] == 3):
+            return states[thrower]
+        states[thrower][4] += 1
+    elif value == "20":
+        if (states[thrower][5] == 3) & (states[1 if thrower == 0 else 0][5] != 3):
+            states[thrower][7] += 20
+            return states[thrower]
+        elif (states[thrower][5] == 3) & (states[1 if thrower == 0 else 0][5] == 3):
+            return states[thrower]
+        states[thrower][5] += 1
+    elif value == "25":
+        if (states[thrower][6] == 3) & (states[1 if thrower == 0 else 0][6] != 3):
+            states[thrower][7] += 25
+            return states[thrower]
+        elif (states[thrower][6] == 3) & (states[1 if thrower == 0 else 0][6] == 3):
+            return states[thrower]
+        states[thrower][6] += 1
     return 0
 
 
 def throw():
-    throw = input("Throw Value: ")
-    updateBoard(throw)
+    score = input("Throw Value: ")  # From gui
+    space = -1
+    for i in range(len(score)):
+        if score[i] == ' ':
+            space = i
+    value = score
+    multiplier = 1
+    if space != -1:
+        value = score[:space]
+        multiplier = int(score[space + 1:])
+    for i in range(multiplier):
+        updateBoard(value)
     printBoard()
 
 
+def undo():  # Watch out, this is permanent
+    p = turn % 2
+    states[p] = boardHistories[p][-2]
+    boardHistories[p] = boardHistories[p][start:-1]
+
+
 def printBoard():
-    if turn % 2 == 0:
-        print(player1 + ":")
-        print("15:", boardState1[0])
-        print("16:", boardState1[1])
-        print("17:", boardState1[2])
-        print("18:", boardState1[3])
-        print("19:", boardState1[4])
-        print("20:", boardState1[5])
-        print("Bulls eye:", boardState1[6])
-        print("Score:", boardState1[7])
-    else:
-        print(player2 + ":")
-        print("15:", boardState2[0])
-        print("16:", boardState2[1])
-        print("17:", boardState2[2])
-        print("18:", boardState2[3])
-        print("19:", boardState2[4])
-        print("20:", boardState2[5])
-        print("Bulls eye:", boardState2[6])
-        print("Score:", boardState2[7])
+    p = turn % 2
+    print(players[p] + "'s scores:")
+    print("15:", states[p][0])
+    print("16:", states[p][1])
+    print("17:", states[p][2])
+    print("18:", states[p][3])
+    print("19:", states[p][4])
+    print("20:", states[p][5])
+    print("Bullseye:", states[p][6])
+    print("Score:", states[p][7])
 
 
 def playGame():
-    while (boardState1[0:7] != [3, 3, 3, 3, 3, 3, 3]) & (boardState2[0:7] != [3, 3, 3, 3, 3, 3, 3]):
+    global subTurn
+    global turn
+    while (boardState1[0:7] != [3, 3, 3, 3, 3, 3, 3]) and (boardState2[0:7] != [3, 3, 3, 3, 3, 3, 3]):
         throw()
-        global subTurn
-        global turn
         subTurn += 1
         if subTurn % 3 == 0:
             subTurn -= 3
@@ -157,7 +120,7 @@ def playGame():
         while boardState2[0:7] != [3, 3, 3, 3, 3, 3, 3]:
             if boardState1[7] >= boardState2[7]:
                 print(player1 + " wins!")
-                break
+                return
             else:
                 throw()
                 subTurn += 1
@@ -179,5 +142,4 @@ def playGame():
         print(player1 + " wins!")
 
 
-
-playGame()
+#playGame()
